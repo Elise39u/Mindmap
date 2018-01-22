7530f9b3c57470311f6c5e18288b4a805edcb4cd
Justin van de laar <justin555@live.nl>
2018-01-22 14:33:59 +0100

    First 5 textareas are in place

---
+++ application/views/index.php
@@ -0,0 +1,16 @@
+<div id="container">
+    <?php
+        $i = 0;
+        while($i < 5) { ?>
+            <textarea class="" id="textarea<?= $i ?>"><?= $i ?></textarea>
+       <?php
+          $i++;
+        }
+    ?>
+    <svg class="ball-container">
+        <circle cx="150" cy="150" r="80" stroke="black" stroke-width="3" fill="white" />
+        <text fill="#000" font-size="45" font-family="Verdana"
+              x="100" y="165">Test</text>
+        Sorry, your browser does not support inline SVG.
+        </svg>
+</div>
